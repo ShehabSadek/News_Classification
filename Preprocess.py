09ffa1b589cbf1903c81bfc005164e633d039028
shehab sadek <shehab.m.sadek@gmail.com>
2023-05-22 19:48:50 +0300

    feat: changes

--- Preprocess.py
+++ Preprocess.py
@@ -7,20 +7,22 @@ nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
 
-def preprocess_text(text):
-    tokens = word_tokenize(text)
-    
-    stop_words = set(stopwords.words('english'))
-    filtered_tokens = [token for token in tokens if token.lower() not in stop_words]
-    
-    filtered_tokens = [token for token in filtered_tokens if token.isalpha()]
-    
-    filtered_tokens = [token.lower() for token in filtered_tokens]
-    
-    lemmatizer = WordNetLemmatizer()
-    filtered_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
-    
-    preprocessed_text = ' '.join(filtered_tokens)
+def preprocess_text(words):
+    preprocessed_text=[]
+    for text in words:
+        tokens = word_tokenize(text)
+        
+        stop_words = set(stopwords.words('english'))
+        filtered_tokens = [token for token in tokens if token.lower() not in stop_words]
+        
+        filtered_tokens = [token for token in filtered_tokens if token.isalpha()]
+        
+        filtered_tokens = [token.lower() for token in filtered_tokens]
+        
+        lemmatizer = WordNetLemmatizer()
+        filtered_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+        
+        preprocessed_text.append(' '.join(filtered_tokens))
     
     return preprocessed_text
 
