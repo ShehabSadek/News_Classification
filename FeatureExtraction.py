6549c15f7f7e15e33c829a48ce37f0c56ab6187f
Amromnia <amr2000225@miuegypt.edu.eg>
2023-05-28 20:16:10 +0300

    Added things and stuff

--- FeatureExtraction.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk import ngrams
-from nltk.tokenize import word_tokenize
-from gensim.models import KeyedVectors
-
-
-def word2vec():
-    # 7ad y3mlha i give up
-    return
--- Preprocess.py
+++ Preprocess.py
@@ -11,36 +11,17 @@ def preprocess_text(words):
     preprocessed_text=[]
     for text in words:
         tokens = word_tokenize(text)
-        
+
         stop_words = set(stopwords.words('english'))
         filtered_tokens = [token for token in tokens if token.lower() not in stop_words]
-        
+
         filtered_tokens = [token for token in filtered_tokens if token.isalpha()]
-        
+
         filtered_tokens = [token.lower() for token in filtered_tokens]
-        
+
         lemmatizer = WordNetLemmatizer()
         filtered_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
-        
+
         preprocessed_text.append(' '.join(filtered_tokens))
-    
+
     return preprocessed_text
-
-
-
-def voc(corpus):
-
-    tokens = nltk.word_tokenize(corpus)
-
-    freq_dist = FreqDist(tokens)
-
-    sorted_words = sorted(freq_dist.items(), key=lambda x: x[1], reverse=True)
-
-    freq_dist.plot(30, cumulative=False)  
-
-    voc_size = 5000 
-
-    top_words = [word for word, freq in sorted_words[:voc_size]]
-
-    print("Vocabulary Size:", len(top_words))
-    print("Top Words:", top_words)
